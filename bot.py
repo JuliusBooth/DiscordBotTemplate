8d5138bda5579804bfd153a94cdf517c86e4df14
Julius Booth <boothjulius@gmail.com>
2023-03-03 12:34:33 +0000

    use bot

--- bot.py
+++ bot.py
@@ -1,28 +1,36 @@
+from discord.ext import commands
 import discord
 import os
 from dotenv import load_dotenv
+from getpass import getpass
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+if os.getenv('DISCORD_TOKEN') is None:
+    TOKEN = getpass("Discord Token: ")
+else:
+    TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
     print('Connected!')    
 
-
-@client.event
+@bot.event
 async def on_message(message):
     # Ignore messages from the bot itself
-    if message.author == client.user:
+    if message.author == bot.user:
         return
-
     # If the message is "ping", respond with "pong"
     if message.content == 'ping':
         await message.channel.send('pong')
+    # process all commands
+    await bot.process_commands(message)
             
+@bot.command(pass_context=True, name='repeat')
+async def repeat(ctx, *args):
+    await ctx.send('_'.join(args))
 
-client.run(TOKEN)
+bot.run(TOKEN)
